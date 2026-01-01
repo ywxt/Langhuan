73a2cc6b9072200e1a2d0bbf18f40c361106a40e
ywxt <ywxtcwh@qq.com>
2026-01-01 15:37:37 +0800

    Remove IListFetcher

--- Langhuan.Core/Langhuan.Core/Fetchers/IFetcher.cs
+++ Langhuan.Core/Langhuan.Core/Fetchers/IFetcher.cs
@@ -6,31 +6,3 @@ public interface IFetcher<in TR, TS>
 {
     Task<TS> FetchAsync(TR request, CancellationToken cancellationToken = default);
 }
-
-public interface IListFetch<T>
-{
-    Task<IEnumerable<T>> FetchListAsync(string id, int page, CancellationToken cancellationToken = default);
-
-    async IAsyncEnumerable<T> FetchAllAsync(string id,
-        [EnumeratorCancellation] CancellationToken cancellationToken = default)
-    {
-        var page = 0;
-        while (page >= 0)
-        {
-            if (cancellationToken.IsCancellationRequested)
-                yield break;
-            var items = await FetchListAsync(id, page, cancellationToken);
-            var hasItems = false;
-            foreach (var item in items)
-            {
-                hasItems = true;
-                yield return item;
-            }
-
-            if (!hasItems)
-                yield break;
-
-            page++;
-        }
-    }
-}
